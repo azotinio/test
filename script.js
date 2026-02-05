57339a73c4eab00b53369bc05a7182ef4cec5fd8
zazat <azat07baltash@gmail.com>
2026-02-05 20:42:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+        function changeContent(){
+                  document.getElementByID("azat").src="fireworks.gif"
+                  document.getElementByid("mygirl").textcontent="Yay!!!"
+        }
