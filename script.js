f0da0812f53f56b897d8f2d6d2cae36ba27d75c7
zazat <azat07baltash@gmail.com>
2026-02-05 20:59:03 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
         function changeContent(){
                   document.getElementByID("azat").src="fireworks.gif"
-                  document.getElementByid("mygirl").textcontent="Yay!!!"
-
         }
 document.getElementById("yesBtn").addEventListener("click", changeContent);
+
